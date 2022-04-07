7ab7a1a4618b993b3205a7a932d67c83a7c0ec56
Michael Guirguis <guirguis@mguirguis.com>
2022-04-07 13:33:54 -0400

    Initial commit

---
+++ src/Project/Genspark/Main.java
@@ -0,0 +1,31 @@
+package Project.Genspark;
+
+import java.util.Scanner;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String intro = "You are in a land full of dragons. In front of you %nyou see two caves. In one cave, the dragon is friendly %nand will share his treasure with you. The other dragon %nis greedy and hungry and will eat you on sight.%n";
+        String badOutcome = "You approach the cave ...%nIt is dark and spooky ...%nA large dragon jumps out in front of you! He opens his jaws and ... %nGobbles you down in one bite!";
+        String goodOutcome = "You approach the cave ...%nYou see a sleeping dragon ...%nYou pass without incident";
+        System.out.print(String.format(intro));
+
+        String answer;
+        do {
+            System.out.println("Which cave will you go into?");
+            answer = in.nextLine();
+        } while(!answer.equals("0") && !answer.equals("1"));
+
+        if (answer.equals("1")) {
+            System.out.print(String.format(badOutcome));
+        } else {
+            System.out.println(String.format(goodOutcome));
+        }
+
+        System.exit(0);
+    }
+}
+

d6099db5879fbaa0434b850ea56ca151da0074cb
Michael Guirguis <guirguis@mguirguis.com>
2022-04-07 17:10:44 -0400

    Extracted out game state into enum

--- src/Project/Genspark/Main.java
+++ src/Project/Genspark/Main.java
@@ -3,8 +3,10 @@ package Project.Genspark;
 import java.util.Scanner;
 
 public class Main {
+    public enum State{
+        WON, LOST, NULL, INVALID
+    }
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         String intro = "You are in a land full of dragons. In front of you %n" +
                 "you see two caves. In one cave, the dragon is friendly %n" +
                 "and will share his treasure with you. " +
@@ -18,24 +20,37 @@ public class Main {
                 "You pass without incident";
         System.out.print(String.format(intro));
 
+        Scanner scanner = new Scanner(System.in);
+
+        State gameState = State.NULL;
         String answer = "";
         do {
             System.out.println("Which cave will you go into?");
             try{
-                answer = in.nextLine();
+                answer = scanner.nextLine();
             } catch (IllegalStateException e){
                 System.out.println("Error: Java scanner is closed; " + e.getMessage());
                 System.exit(1);
             }
-        } while(!answer.equals("0") && !answer.equals("1"));
+            gameState = determineGameState(answer);
+        } while(gameState == State.INVALID);
 
-        if (answer.equals("1")) {
+        if (gameState == State.LOST) {
             System.out.print(String.format(badOutcome));
         } else {
             System.out.println(String.format(goodOutcome));
         }
+    }
 
-        System.exit(0);
+    private static State determineGameState(String answer) {
+        State gameState;
+        if(answer.equals("0"))
+            gameState = State.WON;
+        else if (answer.equals("1"))
+            gameState = State.LOST;
+        else
+            gameState = State.INVALID;
+        return gameState;
     }
 }
 

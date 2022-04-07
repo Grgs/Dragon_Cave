e2913f456a315daedba74f3b28683b4c5d3427c5
Michael Guirguis <guirguis@mguirguis.com>
2022-04-07 16:23:06 -0400

    minor formatting

--- src/Project/Genspark/Main.java
+++ src/Project/Genspark/Main.java
@@ -3,14 +3,19 @@ package Project.Genspark;
 import java.util.Scanner;
 
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String intro = "You are in a land full of dragons. In front of you %nyou see two caves. In one cave, the dragon is friendly %nand will share his treasure with you. The other dragon %nis greedy and hungry and will eat you on sight.%n";
-        String badOutcome = "You approach the cave ...%nIt is dark and spooky ...%nA large dragon jumps out in front of you! He opens his jaws and ... %nGobbles you down in one bite!";
-        String goodOutcome = "You approach the cave ...%nYou see a sleeping dragon ...%nYou pass without incident";
+        String intro = "You are in a land full of dragons. In front of you %n" +
+                "you see two caves. In one cave, the dragon is friendly %n" +
+                "and will share his treasure with you. " +
+                "The other dragon %nis greedy and hungry and will eat you on sight.%n";
+        String badOutcome = "You approach the cave ...%n" +
+                "It is dark and spooky ...%n" +
+                "A large dragon jumps out in front of you! He opens his jaws and ... %n" +
+                "Gobbles you down in one bite!";
+        String goodOutcome = "You approach the cave ...%n" +
+                "You see a sleeping dragon ...%n" +
+                "You pass without incident";
         System.out.print(String.format(intro));
 
         String answer = "";
